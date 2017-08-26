ec283121e5217ff6d8abff50fba2be8cba563df8
Doug Woodward <woodw116@mail.chapman.edu>
2017-08-26 12:45:15 -0700

    week4

---
+++ week4/alterStars.sql
@@ -0,0 +1,8 @@
+ALTER TABLE Star
+ADD COLUMN ra FLOAT,
+ADD COLUMN decl FLOAT;
+
+DELETE  FROM Star;
+
+COPY Star (kepler_id, t_eff, radius, ra, decl)
+  FROM 'stars_full.csv' CSV;
---
+++ week4/updateDelete.sql
@@ -0,0 +1,5 @@
+UPDATE Planet
+SET kepler_name = NULL
+WHERE status != 'CONFIRMED';
+
+DELETE FROM Planet WHERE radius < 0;
